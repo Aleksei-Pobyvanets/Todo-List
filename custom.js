3ed517d987b784e66a593da0dfe510e94610aba9
Alex Pobyvanets <alekseipobyvanets@gmail.com>
2020-02-22 20:29:32 +0200

    js

--- custom.js
+++ custom.js
@@ -9,3 +9,48 @@ function historyOpenButton() {
   function deletedOpenButton() {
     document.getElementById("deletedBTN").classList.toggle('active');
   }
+
+// <div class="history__container" id="historyBTN">
+//     <input type="text" class="history__container inp">
+//     <button class="act__btn ggBTN" id="act__btn">Activate</button>
+//     <button class="delet__btn ggBTN" id="delet__btn">Delete</button>
+// </div>
+// <div class="activate__container" id="activateBTN">
+//     <input type="text" class="active__container inp">
+//     <button class="act__btn ggBTN" id="act__btn">Activate</button>
+//     <button class="delet__btn ggBTN" id="delet__btn">Delete</button>  
+// </div>
+// <div class="deleted__container" id="deletedBTN">
+//     <input type="text" class="deleted__container inp">
+//     <button class="act__btn ggBTN" id="act__btn">Activate</button>
+//     <button class="delet__btn ggBTN" id="delet__btn">Delete</button>   
+// </div>
+
+var list = document.querySelector('ul');
+list.addEventListener('click', function(ev){
+  if(ev.target.tagName === 'LI'){
+    ev.target.classList.toggle('clecked');
+  } else if(ev.target.tagName === 'SPAN'){
+    var div = ev.target.parentNode;
+    div.remove();
+  }
+}, false);
+
+function newElement(){
+  var li = document.createElement('li');
+  li.classList.toggle('border')
+  var inputValue = document.getElementById('textArea__searsch').value;
+  var newEl = document.createTextNode(inputValue);
+  li.appendChild(newEl);
+    if(inputValue === ""){
+        alert("Введите текст")
+    } else {
+        document.getElementById('list').appendChild(li);
+    }
+    document.getElementById('textArea__searsch').value = "";
+    var span = document.createElement('SPAN');
+    var text = document.createTextNode('X');
+    span.className = 'close';
+    span.appendChild(text);
+    li.appendChild(span);
+}
