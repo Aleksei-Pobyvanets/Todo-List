4f4a4edcbc570308e82eb88133b2dc7fee0e90e4
Alex Pobyvanets <alekseipobyvanets@gmail.com>
2020-02-22 15:04:12 +0200

    JS

---
+++ custom.js
@@ -0,0 +1,11 @@
+function historyOpenButton() {
+    document.getElementById("historyBTN").classList.toggle('active');
+  }
+
+  function activateOpenButton() {
+    document.getElementById("activateBTN").classList.toggle('active');
+  }
+  
+  function deletedOpenButton() {
+    document.getElementById("deletedBTN").classList.toggle('active');
+  }
