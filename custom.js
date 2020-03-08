63a4bbcb6c73cdb61fc80fe5c4b20fc35f57c033
Alex Pobyvanets <alekseipobyvanets@gmail.com>
2020-03-08 13:57:38 +0200

    DRAG n DROP

--- custom.js
+++ custom.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", function(event) {
+document.addEventListener("DOMContentLoaded", function (event) {
   console.log("DOM fully loaded and parsed");
 
   var data = {
@@ -6,137 +6,156 @@ document.addEventListener("DOMContentLoaded", function(event) {
     complete: []
   }
 
-  document.getElementById('add').addEventListener('click', function(){
-  var value = document.getElementById('item').value;
-    if(value) {
+  function chekInp() {
+    var value = document.getElementById('item').value;
+    if (value === '') {
+      return false
+    }
+    if (value) {
       data.todo.push(value);
       addItemTodo(value);
       var value = document.getElementById('item').value = "";
     }
-  });
-  document.getElementById('item').addEventListener('keydown', function(e){
-  var value = this.value
-  if(e.keyCode === 13){
-    data.todo.push(value);
-    addItemTodo(value);
-    var value = document.getElementById('item').value = "";
   }
+  document.getElementById('add').addEventListener('click', function () {
+    chekInp();
+  });
+  document.getElementById('item').addEventListener('keydown', function (e) {
+    if (e.keyCode === 13) {
+      chekInp();
+    }
   })
 
-function removeItem(){
-  var item = this.parentNode.parentNode;
-  var parent = item.parentNode;
-  var id = parent.id;
-  var value = item.innerText;
+  function removeItem() {
+    var item = this.parentNode.parentNode;
+    var parent = item.parentNode;
+    var id = parent.id;
+    var value = item.innerText;
 
-  if(id === 'todo'){
-    data.todo.splice(data.todo.indexOf(value), 1);
-  }else{
-    data.complete.splice(data.complete.indexOf(value), 1);
+    if (id === 'todo') {
+      data.todo.splice(data.todo.indexOf(value), 1);
+    } else {
+      data.complete.splice(data.complete.indexOf(value), 1);
+    }
+
+    parent.removeChild(item);
   }
-  
-  parent.removeChild(item);
-}
-function completeItem(){
-  var item = this.parentNode.parentNode;
-  var parent = item.parentNode;
-  var id = parent.id;
-  var value = item.innerText;
-  if(id === 'todo'){
-    data.todo.splice(data.todo.indexOf(value), 1);
-    data.complete.push(value);
-  }else{
-    data.complete.splice(data.complete.indexOf(value), 1);
-    data.todo.push(value);
+
+  function completeItem() {
+    var item = this.parentNode.parentNode;
+    var parent = item.parentNode;
+    var id = parent.id;
+    var value = item.innerText;
+    if (id === 'todo') {
+      data.todo.splice(data.todo.indexOf(value), 1);
+      data.complete.push(value);
+    } else {
+      data.complete.splice(data.complete.indexOf(value), 1);
+      data.todo.push(value);
+    }
+
+
+    var target = (id === 'todo') ? target = document.getElementById('completed') : target = document.getElementById('todo');
+    parent.removeChild(item);
+    target.insertBefore(item, target.childNodes[0]);
   }
-  
 
-  var target = (id === 'todo') ? target = document.getElementById('completed') : target = document.getElementById('todo');
-  parent.removeChild(item);
-  target.insertBefore(item, target.childNodes[0]);
-}
+  function buttons(todoItem) {
+    function remove() {
+      var remove = document.createElement('button');
+      remove.classList.add('remove');
+      remove.classList.add('far');
+      remove.classList.add('fa-trash-alt');
+      remove.addEventListener('click', removeItem)
+      return remove
+    }
 
-function addItemTodo(text){
-  var list = document.getElementById('todo');
-  var divLI = document.createElement('div')
-  var item = document.createElement('li');
-  divLI.classList.add('divLI');
-  item.classList.add('listLi');
-  divLI.setAttribute('draggable', true);
-  divLI.setAttribute('id', 'item')
-  item.innerText = text;
+    function complete() {
+      var complete = document.createElement('button');
+      complete.classList.add('complete');
+      complete.classList.add('far');
+      complete.classList.add('fa-check-circle');
+      complete.addEventListener('click', completeItem);
+      return complete
+    }
+    var buttons = document.createElement('div');
+    buttons.classList.add('buttons');
+    var complete = complete();
+    var remove = remove();
+    buttons.appendChild(remove);
+    buttons.appendChild(complete);
+    todoItem.appendChild(buttons);
+  }
 
-  var buttons = document.createElement('div');
-  buttons.classList.add('buttons');
+  function createTodoEl(text) {
+    var item = document.createElement('li');
+    item.classList.add('listLi');
+    item.classList.add('draggable')
+    item.setAttribute('draggable', true);
+    item.innerText = text;
+    addEventsDragAndDrop(item);
+    return item;
+  }
 
-  var remove = document.createElement('button');
-  remove.classList.add('remove');
-  remove.classList.add('far');
-  remove.classList.add('fa-trash-alt');
-  remove.addEventListener('click', removeItem)
+  function addItemTodo(text) {
+    console.log(text)
+    var parent = document.getElementById('todo');
 
-  var complete = document.createElement('button');
-  complete.classList.add('complete');
-  complete.classList.add('far');
-  complete.classList.add('fa-check-circle');
+    var todoItem = createTodoEl(text);
+    buttons(todoItem);
 
-  complete.addEventListener('click', completeItem)
-  buttons.appendChild(remove);
-  buttons.appendChild(complete);
-  item.appendChild(buttons);
-  divLI.appendChild(item)
-
-  list.insertBefore(item, list.childNodes[0]);
-
-}
-
-
-// Drag n Drop
-// const list_items = document.querySelectorAll('#item');
-// const lists = document.querySelectorAll('.Dr');
-
-
-// var draggedItem = null;
-
-// for(var i = 0; i < list_items.length; i++){
-//     const itemDR = list_items[i];
-
-//     itemDR.addEventListener('dragstart', function(){
-//       draggedItem = itemDR;
-//       setTimeout(function (){
-//         itemDR.style.display = 'none';
-//         }, 0);
-//     });
-//     itemDR.addEventListener('dragend', function(){
-//       setTimeout(function (){
-//         draggedItem.style.display = 'block';
-//         draggedItem = null;
-//       }, 0);
-//     });
-
-//     for(var j = 0; j < lists.length; j++){
-//       const list = lists[j];
-
-//       list.addEventListener('dragover', function (e) {
-//         e.preventDefault();
-//       })
-//       list.addEventListener('dragenter', function (e) {
-//         e.preventDefault();
-//         this.style.backgroundColor = '#7C7C7C';
-//       })
-//       list.addEventListener('drop', function () {
-//         this.append(draggedItem);
-//         this.style.backgroundColor = '#AEAEAE';
-//       })
-//     }
-// }
+    todo.appendChild(todoItem);
+    parent.insertBefore(todoItem, parent.childNodes[0]);
+  }
 
 
-document.getElementById('todo-btn').addEventListener('click', function(){
-  document.getElementById("todoBtn").classList.toggle('Active');
-})
-document.getElementById('complete-btn').addEventListener('click', function(){
-  document.getElementById("completeBtn").classList.toggle('Active');
-})
+  //Drag n Drop
+  function dragStart(e) {
+    dragSrcEl = this;
+    e.dataTransfer.effectAllowed = 'move';
+    e.dataTransfer.setData('text/html', this.innerHTML);
+  };
+
+  function dragOver(e) {
+    e.preventDefault();
+    e.dataTransfer.dropEffect = 'move';
+    return false;
+  }
+
+  function dragDrop(e) {
+    if (dragSrcEl != this) {
+      dragSrcEl.innerHTML = this.innerHTML;
+      this.innerHTML = e.dataTransfer.getData('text/html');
+    }
+    return false;
+  }
+
+  function dragEnd(e) {
+    var listItens = document.querySelectorAll('.draggable');
+    [].forEach.call(listItens, function (item) {
+      item.classList.remove('over');
+    });
+  }
+
+  function addEventsDragAndDrop(el) {
+    el.addEventListener('dragstart', dragStart, false);
+    el.addEventListener('dragover', dragOver, false);
+    el.addEventListener('drop', dragDrop, false);
+  }
+  var listItens = document.querySelectorAll('li');
+  console.log(listItens);
+  [].forEach.call(listItens, function (item) {
+    addEventsDragAndDrop(item);
+  });
+
+
+
+
+  document.getElementById('todo-btn').addEventListener('click', function () {
+    document.getElementById("todoBtn").classList.toggle('Active');
+  })
+  document.getElementById('complete-btn').addEventListener('click', function () {
+    document.getElementById("completeBtn").classList.toggle('Active');
+  })
 
 });
